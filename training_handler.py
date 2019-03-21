dc27279a9213100d360da8300fa228cea9ac3d92
Raghu Vamsi Chittersu <raghu.krishna8@gmail.com>
2019-03-22 03:33:21 +0530

    progressed on training handler

--- training_handler.py
+++ training_handler.py
@@ -1,36 +1,53 @@
 from encoder import Encoder
 from decoder import Decoder
+from torchtext.data import Field, BucketIterator, TabularDataset
+import torch
 
 
 class Trainer:
 
-    def __init__(self, encoder, decoder):
+    def __init__(self, lang1, lang2, encoder, decoder, mode, key):
+        self.lang1 = lang1
+        self.lang2 = lang2
         self.encoder: Encoder = encoder
         self.decoder: Decoder = decoder
+        self.mode = mode
+        self.key = key
+        self.result_file_name = key+mode + '.res'
 
-    def feed_mini_batch(self, input_tensor, input_lengths, input_mask,  output_tensor, output_lengths,
-                        output_mask, train=True, result_file=None, attention_file=None):
-        if train:
+    def feed_mini_batch(self, input_tensor,  output_tensor):
+        # operation such that all non zero is one and all one is zero
+        input_mask = input_tensor.clone().apply_(lambda val: 0 if val == 1 else 1)
+        input_lengths = torch.sum(input_mask, 0)
+
+        if self.mode == 'train':
             self.encoder.reset_grad()
             self.decoder.reset_grad()
 
         hidden_states, hidden_state, cell_state = self.encoder(input_tensor, input_lengths)
-        loss, result, attn = self.decoder(output_tensor, hidden_states, input_mask, hidden_state, cell_state)
+        loss, result, _ = self.decoder(output_tensor, hidden_states, input_mask, hidden_state, cell_state)
 
-        if train:
+        if self.mode == 'train':
             loss.backward()
             self.encoder.update_weights()
             self.decoder.update_weights()
 
-        # if result_file is not None:
-        #     # write the translated sentence to file
-        #     pass
-        #
-        # if attention_file is not None:
-        #     # write attention to file
-        #     pass
+        if self.mode == 'eval' or self.mode == 'test':
+            # write translated stuff to file
+            EN_TEXT.vocab.itos[11]
+            pass
 
-    def train(self, source_language_file, destination_language_file):
-        # from file read a mini batch
-        #
-        pass
+    def run(self, file, batch_size, batches):
+
+        data_fields = [('lang1', self.lang1), ('lang2', self.lang2)]
+        data = TabularDataset(path='./train.en.de', train=file, format='tsv', fields=data_fields, skip_header=True)
+
+        batch_iterator = BucketIterator(data, batch_size=batch_size, sort_key=lambda x: len(x.lang1),
+                                        shuffle=True, sort_within_batch=True, repeat=False)
+
+        for i in range(batches):
+            try:
+                batch = next(iter(batch_iterator))
+                self.feed_mini_batch(batch.lang1, batch.lang2)
+            except StopIteration:
+                break
